c305813b38b5d0bea7531d09e7fddbc0076bbf88
Psyclin <darrenjberg@gmail.com>
2025-01-04 08:41:17 -0700

    Fixing linking issue

--- js/main.js
+++ js/main.js
@@ -1093,12 +1093,12 @@ jQuery('.wf-item').on('click', function() {
 })();
 
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('header.html')
+    fetch('/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
         });
-    fetch('footer.html')
+    fetch('/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer').innerHTML = data;
